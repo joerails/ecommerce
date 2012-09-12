077843fa3ce1d978e7de012702a1c29c03d590e5
vasundhara <vasundhara@weboniselab.com>
2012-09-12 13:33:42 +0530

    code fix

--- app/overrides/tokens.rb
+++ app/overrides/tokens.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_products",
-                     :name => "tokens",
-                     :replace => "ul.product-listing",
-                     :partial => 'shared/buy_now')
+#Deface::Override.new(:virtual_path  => "spree/shared/_products",
+#                     :name => "tokens",
+#                     :replace => "ul.product-listing",
+#                     :partial => 'shared/buy_now')
